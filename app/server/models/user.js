7b9607a93f19d8748de59e951512e0ff0c4fc918
Wilson Hsu <wilsonhsu0102@gmail.com>
2021-10-18 14:05:05 -0400

    added freezeTableName to user table

--- app/server/models/user.js
+++ app/server/models/user.js
@@ -9,7 +9,12 @@ module.exports = (sequelize, Sequelize) => {
         age: {
             type: Sequelize.INTEGER
         }
-    });
+    }, {
+        // disable the modification of table names; By default, sequelize will automatically
+        // transform all passed model names (first parameter of define) into plural.
+        // if you don't want that, set the following
+        freezeTableName: true,
+      });
 
     return User;
 };
