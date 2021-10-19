998d31b493d3d7e21873fdde30174ec29f7c46d8
Wilson Hsu <wilsonhsu0102@gmail.com>
2021-10-18 20:12:01 -0400

    changed sequelize to use migration approach

--- app/server/models/index.js
+++ app/server/models/index.js
@@ -1,41 +1,39 @@
-const dbConfig = require("../configs/db.config.js");
+'use strict';
 
-const Sequelize = require("sequelize");
-let isLocal = process.env.SERVER_ENV === undefined;
-
-const sequelize = !isLocal ? 
-    new Sequelize(process.env.DATABASE_URL, {
-        pool: {
-            max: dbConfig.pool.max,
-            min: dbConfig.pool.min,
-            acquire: dbConfig.pool.acquire,
-            idle: dbConfig.pool.idle
-        },
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
-    }) :
-    new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-        host: dbConfig.HOST,
-        dialect: dbConfig.dialect,
-        operatorsAliases: false,
-
-        pool: {
-            max: dbConfig.pool.max,
-            min: dbConfig.pool.min,
-            acquire: dbConfig.pool.acquire,
-            idle: dbConfig.pool.idle
-        }
-    });
-
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.SERVER_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-db.users = require("./user.model.js")(sequelize, Sequelize);
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.users = require("./user.js")(sequelize, Sequelize);
 
 module.exports = db;
