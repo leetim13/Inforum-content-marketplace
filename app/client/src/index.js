6af7d217b53655781115b9df48ee138e8a7cf4f1
Wilson Hsu <wilsonhsu0102@gmail.com>
2021-10-26 20:06:25 -0400

    added sentry to client app

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+Sentry.init({
+  dsn: "https://7dc304ef696c469589d0bb299f785b3f@o358880.ingest.sentry.io/6036649",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
