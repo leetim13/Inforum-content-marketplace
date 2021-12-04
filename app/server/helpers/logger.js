82d1226aecafc33f26669ec961edf93b862bff95
Wilson Hsu <wilsonhsu0102@gmail.com>
2021-12-04 17:28:11 -0500

    use localConfig logger key

--- app/server/helpers/logger.js
+++ app/server/helpers/logger.js
@@ -5,7 +5,7 @@ var options = {
     tags: ['logging', 'nodejs', 'logdna'] // Tags can also be provided in comma-separated string format: 'logging,nodejs,logdna'    
 };
 
-const logKey = process.env.LOGDNA_KEY ? process.env.LOGDNA_KEY[0] : require('../config/localConfig.json').logDNA.key;
+const logKey = require('../config/localConfig.json').logDNA.key;
 const logger = Logger.createLogger(logKey, options);
 exports.info = (msg) => {
     logger.info(msg);
