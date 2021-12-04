01d348549d38fdd67743d410bf1a4ed1dde912a0
Wilson Hsu <wilsonhsu0102@gmail.com>
2021-12-04 17:37:12 -0500

    update logDNA key

--- app/server/helpers/logger.js
+++ app/server/helpers/logger.js
@@ -5,9 +5,8 @@ var options = {
     tags: ['logging', 'nodejs', 'logdna'] // Tags can also be provided in comma-separated string format: 'logging,nodejs,logdna'    
 };
 
-const logKey = require('../config/localConfig.json').logDNA.key;
-console.log(logKey);
-console.log(process.env.LOGDNA_KEY[0])
+const logKey = process.env.LOGDNA_KEY ? JSON.parse(process.env.LOGDNA_KEY)[0] : require('../config/localConfig.json').logDNA.key;
+console.log(JSON.parse(process.env.LOGDNA_KEY)[0])
 const logger = Logger.createLogger(logKey, options);
 exports.info = (msg) => {
     logger.info(msg);
