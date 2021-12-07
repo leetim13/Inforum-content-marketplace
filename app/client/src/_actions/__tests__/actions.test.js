4f08b5ed31d6f1cab668e3ce867790b05994f0c8
Timothy Lee <hwtimothylee@yahoo.com>
2021-12-07 15:38:52 -0500

    removed bank.actions.js

--- app/client/src/_actions/__tests__/actions.test.js
+++ app/client/src/_actions/__tests__/actions.test.js
@@ -6,8 +6,17 @@ import { alertActions, campaignActions, postActions} from '..';
 
 const dispatch = jest.fn();
 
+test('alertActions', () => {
+  const props = alertActions.success(dispatch);
+  expect(props).toEqual({"message": expect.any(Function), "type": "ALERT_SUCCESS"})
+})
+
 test('postActions', () => {
   const props = postActions.getAll(dispatch);
   expect(props).toEqual(expect.any(Function))
 })
 
+test('campaignActions', () => {
+  const props = campaignActions.updateCampaigns(dispatch);
+  expect(props).toEqual(expect.any(Function))
+})
